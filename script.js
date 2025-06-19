9a4426d101377eba27c626942a2e46023d3c6164
kavya agrawal <kavya.agrawal.ug24@nsut.ac.in>
2025-06-19 17:54:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById('toggle-mode');
+
+toggleBtn?.addEventListener('click', () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
+});
+
